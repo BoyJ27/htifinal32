eba56f03a36c3cbb824903fd8283f8ef78dcbc42
Janissen <b.n.j.janissen@student.tue.nl>
2017-06-12 12:59:03 +0200

    Updated Mainscreen
    Some things are not functional yet. Biggest issue = making 'pages remember' in what state they are.

--- mainscreen.js
+++ mainscreen.js
@@ -1,12 +1,20 @@
-function updateMain() {
 
-  var time = get('time', 'time');
-  var day = get('day', 'day');
-  var divtime = document.getElementById('currenttime');
-  var divday = document.getElementById('currentday');
-  divtime.id = 'divtime';
-  divday.id = 'divday';
-  divday.append(day);
-  divtime.append(time);
+var myVar;
+$( document ).ready(function() {
+  myVar = setInterval(updateMain, 200);
+});
 
+function updateMain(){
+var time = get("time", "time");
+var day = get("day", "current_day");
+var currtemp = get("currentTemperature", "current_temperature");
+var targettemp = get("targetTemperature", "target_temperature");
+$("#currentday").empty();
+$("#currentday").append(day);
+$("#currenttime").empty();
+$("#currenttime").append(time);
+$("#currentTemp").empty();
+$("#currentTemp").append(currtemp);
+$("#targetTemp").empty();
+$("#targetTemp").append(targettemp);
 }
