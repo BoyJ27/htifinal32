fa41ff301618c77d7eb9190902c36bc8a8e48f7d
filipdavidovic <filip.davidovic.fd@gmail.com>
2017-06-20 11:04:16 +0200

    pie slider

--- mainscreen.js
+++ mainscreen.js
@@ -1,4 +1,39 @@
 $( document ).ready(function() {
+  var object;
+  $("#tempSlider").roundSlider({
+    sliderType: "min-range",
+    handleShape: "dot",
+    circleShape: "pie",
+    width: 24,
+    radius: 160,
+    value: get("currentTemperature", "current_temperature"),
+    startAngle: 315,
+    handleSize: "+14",
+    max: "29.5",
+    min: "5.5",
+    step: "0.1",
+    mouseScrollAction: true,
+    animation: false,
+
+    drag: function (args) {
+        // handle the drag event here
+        sliderColorRefactor(args);
+    },
+    create: function(args) {
+        sliderColorRefactor(args);
+        $("body").append("<style>.rs-overlay { background-color: #cccccc; } .rs-inner { background-color: #cccccc; } .tempbuttons { text-align: left; }</style>");
+    },
+    change: function (args) {
+        // handle the change event here
+        // console.log(args.value);
+        put('targetTemperature', 'target_temperature', args.value);
+        var targettemp = get("targetTemperature", "target_temperature");
+        $("#targetTemp").text(targettemp);
+
+        sliderColorRefactor(args);
+    }
+  });
+
     updateMain();
     var myVar = setInterval(updateMain, 400);
 
@@ -8,71 +43,43 @@ $( document ).ready(function() {
       $("#dynamicimage").attr('src', '');
       $("#dynamicimage").attr('src', 'holidaygrey.png');
     } else if (holidaystate == 'off'){
-      console.log('JAJA!');
       $("#dynamicimage").attr('src', '');
       $("#dynamicimage").attr('src', "holiday.png");
     }
-
-    $("#tempSlider").roundSlider({
-      sliderType: "min-range",
-      handleShape: "dot",
-      width: 24,
-      radius: 160,
-      value: get("currentTemperature", "current_temperature"),
-      startAngle: 270,
-      handleSize: "+14",
-      max: "29.5",
-      min: "5.5",
-      step: "0.1",
-      mouseScrollAction: true,
-
-      drag: function (args) {
-          // handle the drag event here
-          sliderColorRefactor(args);
-      },
-      create: function(args) {
-          sliderColorRefactor(args);
-      },
-      change: function (args) {
-          // handle the change event here
-          // console.log(args.value);
-          put('targetTemperature', 'target_temperature', args.value);
-          var targettemp = get("targetTemperature", "target_temperature");
-          $("#targetTemp").text(targettemp);
-
-          sliderColorRefactor(args);
-      }
-  });
 
   $("#tempupfast").click(function(){
     var currsetting = get("targetTemperature", "target_temperature");
     var parsedsetting = parseFloat(currsetting);
     if (parsedsetting < 29.5){
-    var newsetting = parsedsetting + 0.5;
-    put('targetTemperature', 'target_temperature', newsetting);
-  } else {
-    alert("You have reached the maximum temperature.");
-  }
+      var newsetting = parsedsetting + 0.5;
+      put('targetTemperature', 'target_temperature', newsetting);
+    } else {
+      alert("You have reached the maximum temperature.");
+    }
   });
   $("#tempup").click(function(){
     var currsetting = get("targetTemperature", "target_temperature");
     var parsedsetting = parseFloat(currsetting);
     if (parsedsetting < 29.9){
-    var newsetting = parsedsetting + 0.1;
-    put('targetTemperature', 'target_temperature', newsetting);
-  } else {
-    alert("You have reached the maximum temperature.");
-  }
+      var newsetting = parsedsetting + 0.1;
+      put('targetTemperature', 'target_temperature', newsetting);
+
+      $("#tempSlider").roundSlider("option", "value", newsetting);
+    } else {
+      alert("You have reached the maximum temperature.");
+    }
   });
   $("#tempdown").click(function(){
     var currsetting = get("targetTemperature", "target_temperature");
     var parsedsetting = parseFloat(currsetting);
     if (parsedsetting > 5.1){
-    var newsetting = parsedsetting - 0.1;
-    put('targetTemperature', 'target_temperature', newsetting);
-  } else {
-    alert("You have reached the maximum temperature.");
-  }
+      var newsetting = parsedsetting - 0.1;
+      put('targetTemperature', 'target_temperature', newsetting);
+
+      $("#tempSlider").roundSlider("option", "value", newsetting);
+    } else {
+      alert("You have reached the maximum temperature.");
+    }
 
   });
   $("#tempdownfast").click(function(){
@@ -89,7 +96,7 @@ $( document ).ready(function() {
 
 ColorMix.setGradient([
   { reference: 0, color: { red: 0, green: 0, blue: 255 } },
-  { reference: 25, color: { red: 0, green: 255, blue: 255 } },
+  { reference: 25, color: { red: 0, green: 155, blue: 255 } },
   { reference: 50, color: { red: 255, green: 255, blue: 255 } },
   { reference: 75, color: { red: 255, green: 155, blue: 0 } },
   { reference: 100, color: { red: 255, green: 0, blue: 0 } }
