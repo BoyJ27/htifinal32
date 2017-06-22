0615af4be11c11e13194055489c30079deeb2fc7
Maurits van Riezen <MauritsVanRiezen@gmail.com>
2017-06-22 12:36:40 +0200

    redsesigned temperature selection

--- dayNightChange.js
+++ dayNightChange.js
@@ -1,6 +1,4 @@
-$(document).ready(function() {
-  getCurrent();
-});
+$(document).ready(getCurrent);
 
 function getCurrent() {
   $("#newDayTemp").val(get("dayTemperature", "day_temperature"));
@@ -16,4 +14,23 @@ function submitChanges() {
   }
 
   getCurrent();
+}
+
+function left(type){
+	move(type, -0.1);
+}
+
+function right(type){
+	move(type, 0.1);
+}
+
+function move(type, amount) {
+	$("#new"+type+"Temp").val((Number($("#new"+type+"Temp").val())+amount).toFixed(1));
+	submitChanges();
+}
+
+function resetTemp(){
+	put("dayTemperature", "day_temperature", 25);
+	put("nightTemperature", "night_temperature", 15);
+	getCurrent();
 }
